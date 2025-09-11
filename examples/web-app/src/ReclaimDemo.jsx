c37412c59968df57b30ff86202bec17124425461
sajjad21990 <sajjadhaider21990@gmail.com>
2025-09-11 23:29:37 +0530

    removed unused code

--- examples/web-app/src/ReclaimDemo.jsx
+++ examples/web-app/src/ReclaimDemo.jsx
@@ -3,19 +3,7 @@ import { reclaimExtensionSDK } from '@reclaimprotocol/browser-extension-sdk';
 import './ReclaimDemo.css';
 
 const PROVIDERS = [
-  { id: '9ab972fc-8aca-4c35-93f5-d1ede32e32b9', name: 'Trex - Instagram' },
-  { id: '50a2286d-8c89-4de0-817b-e577447c531d', name: 'Trex - Grok' },
   { id: '7519ad78-208a-425d-9fac-97c13b0f0d4d', name: 'Kaggle' },
-  { id: '25a97f94-4c73-4c02-826d-d11504997fec', name: 'Trex Perplexity' },
-  { id: '218f590e-d755-44c9-82e3-04e9907d3f44', name: 'Trex - Chatgpt' },
-  { id: '32dc2faa-77fa-4af1-a8ed-5df70fdca8dd', name: 'Claude' },
-  { id: '921681c2-3d20-4060-b961-43ae2a0e8dd2', name: 'Gemini' },
-  { id: '6b6d447f-caa7-461c-bd13-5c4738d7b4f9', name: 'Kaggle Injections' },
-  { id: 'c9656893-ab80-4f17-9e88-0bcc33da123b', name: 'Kaggle Cascading' },
-  { id: '31e222ba-be21-4bec-b767-af30f52836d9', name: 'Steam Trade' },
-  { id: '31e222ba-be21-4bec-b767-af30f52837ea', name: 'Steam Inventory' },
-  { id: '214861a3-191b-427b-9862-75e301f1e63b', name: 'Tiktok' },
-  { id: 'fbf83028-fbed-4414-b593-fa5d3e3fa131', name: 'Trex - Binance' },
 ];
 
 const APP_ID = import.meta.env.VITE_RECLAIM_APP_ID;
@@ -92,22 +80,6 @@ export default function ReclaimDemo() {
         extensionID: EXTENSION_ID,
       });
 
-  
-      
-
-      // request.setParams({
-      //   // srivatsan
-      //   username: "76561198886166562",
-      //   // mushaheed
-      //   // username: "white_shadow_x7"
-      // });
-
-    //   request.setParams({
-    //     "theirTradeLink": "https://steamcommunity.com/tradeoffer/new/?partner=482038931&token=7d8YweiW",
-    //     "tradeOfferMessage": "Hello, my first one....",
-    //     "myTradeAssets": "{\"assets\":[],\"currency\":[],\"ready\":false}",
-    //     "theirTradeAssets": "{\"assets\":[{\"appid\":753,\"contextid\":\"6\",\"amount\":\"1\",\"assetid\":\"16773845215\"}],\"currency\":[],\"ready\":false}"
-    // });
 
       setReq(request);
       setStatusUrl(request.getStatusUrl());
